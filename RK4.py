ee3bea607cf1bec70a4a26cb8e9a936cb6ab7d7c
dragowiser <diegoantoniogs97@gmail.com>
2023-10-11 11:14:18 -0600

    RK4 terminado

--- RK4.py
+++ RK4.py
@@ -1,4 +1,5 @@
-#Creamos el método de Runge- Kutta 4 para una ecuación diferencial a partir de una función
-def Runge_Kutta4(f, y_0, h, nPasos):
-    k_1 = h * f(y_0)
-    k_2 = h * f(y_0 + h/2)
+import numpy as np
+
+s = np.zeros(2)
+
+s[0]= [1,2]
